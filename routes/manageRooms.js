92138ae41857d2a2c01638c347d6606a6e6411a6
kuroisepr <kanittha41@gmail.com>
2023-02-22 00:54:09 +0700

    Add Pagination API on Room

--- routes/manageRooms.js
+++ routes/manageRooms.js
@@ -7,6 +7,40 @@ const RoomTypeModel = require('../models/RoomType');
 const OrgModel = require('../models/Org')
 const mongoose = require('mongoose')
 
+function paginatedResults(model) {
+    return async (req, res, next) => {
+        const page = parseInt(req.query.page)
+        const limit = req.query.limit
+
+        const startIndex = (page - 1) * limit
+        const endIndex = page * limit
+
+        const results = {}
+
+        if (endIndex < model.length) {
+            results.next = {
+                page: page + 1,
+                limit: limit
+            }
+        }
+
+        if (startIndex > 0) {
+            results.previous = {
+                page: page - 1,
+                limit: limit
+            }
+        }
+
+        try {
+            results.results = await model.find().limit(limit).skip(startIndex).exec()
+            next()
+        } catch (e) {
+            res.status(500).json({ message: e.message })
+        }
+
+        res.paginatedResults = results
+    }
+}
 router.post('/building', async (req, res) => {
 
     const Organization = await OrgModel.findById(req.body.id)
@@ -168,12 +202,13 @@ router.post('/room', async (req, res) => {
 });
 
 
-router.get('/room', async (req, res) => {
-    RoomsModel.find({}, (err, result) => {
+router.get('/room', paginatedResults(RoomsModel), async (req, res) => {
+
+    RoomsModel.find({}, (err) => {
         if (err) {
             res.send(err)
         } else {
-            res.send(result)
+            res.send(res.paginatedResults)
         }
     })
 })
