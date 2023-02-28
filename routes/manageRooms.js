d5f017c44bc28a6813a79261e11f271284e3999e
kuroisepr <kanittha41@gmail.com>
2023-02-28 21:10:16 +0700

    Room Pagination Res Changed

--- routes/manageRooms.js
+++ routes/manageRooms.js
@@ -15,8 +15,6 @@ function paginatedResults(model) {
         const startIndex = (page - 1) * limit
         const endIndex = page * limit
 
-        const results = {}
-
         if (endIndex < model.length) {
             results.next = {
                 page: page + 1,
@@ -32,7 +30,7 @@ function paginatedResults(model) {
         }
 
         try {
-            results.results = await model.find().limit(limit).skip(startIndex).exec()
+            results = await model.find().limit(limit).skip(startIndex).exec()
             next()
         } catch (e) {
             res.status(500).json({ message: e.message })
@@ -41,6 +39,7 @@ function paginatedResults(model) {
         res.paginatedResults = results
     }
 }
+
 router.post('/building', async (req, res) => {
 
     const Organization = await OrgModel.findById(req.body.id)
